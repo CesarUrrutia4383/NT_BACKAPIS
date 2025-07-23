4ce878ac4f6bb2abc80f5beaab3a5f436a00971d
I_4m_SilverGWolf <ssoto4383@gmail.com>
2025-07-23 14:01:38 -0600

    Ajustes menores a pagina para pruebas y despliegue local

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,11 +4,9 @@ const { pool}= require ('../config/db.js');
 const getAllProducts = async (req, res) => {
   console.log('Solicitud recibida para obtener productos'); // LOG
   try {
-    const [rows] = await pool.query('SELECT * FROM productos');
-    console.log('Productos obtenidos de la base de datos:', rows); // LOG
+    const [rows] = await pool.query('SELECT * FROM productos'); // LOG
     res.json(rows);
-  } catch (error) {
-    console.error('Error al obtener productos:', error); // LOG
+  } catch (error) {// LOG
     res.status(500).json({ message: 'Error fetching products', error });
   }
 };
