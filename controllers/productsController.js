82c477091f469258e6cec91d7516ad06294a82fe
I_4m_SilverGWolf <ssoto4383@gmail.com>
2025-07-28 09:41:38 -0600

    Cor

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,16 +2,15 @@ const { pool }= require ('../config/db.js');
 
 const getAllProducts = async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT codigo_producto, nombre_producto, Marca, Proposito, Existencias, Info, imagen_base64 FROM productos');
+    const [rows] = await pool.query('SELECT codigo_producto, nombre_producto, marca, proposito, existencias, info FROM productos');
     // Mapear los campos a los nombres esperados por el frontend
     const productos = rows.map(row => ({
       id: row.codigo_producto,
       nombre: row.nombre_producto,
-      marca: row.Marca,
-      proposito: row.Proposito,
-      cantidad: row.Existencias,
-      info: row.Info,
-      imagen_base64: row.imagen_base64
+      marca: row.marca,
+      proposito: row.proposito,
+      cantidad: row.existencias,
+      info: row.info
     }));
     res.json(productos);
   } catch (error) {
