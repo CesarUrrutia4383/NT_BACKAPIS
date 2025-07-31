4e285f23126ddbd3c750bcbc6559f1d60f2a3bba
I_4m_SilverGWolf <ssoto4383@gmail.com>
2025-07-31 10:09:48 -0600

    Mejoras visuales ademas de hacer uso de imagenes mediante un APi externo de alamacenamiento

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,7 @@ const { pool }= require ('../config/db.js');
 
 const getAllProducts = async (req, res) => {
   try {
-    const [rows] = await pool.query('SELECT codigo_producto, nombre_producto, marca, proposito, existencias, info FROM productos');
+    const [rows] = await pool.query('SELECT codigo_producto, nombre_producto, marca, proposito, existencias, info, imagen_url FROM productos');
     // Mapear los campos a los nombres esperados por el frontend
     const productos = rows.map(row => ({
       id: row.codigo_producto,
@@ -10,7 +10,8 @@ const getAllProducts = async (req, res) => {
       marca: row.marca,
       proposito: row.proposito,
       cantidad: row.existencias,
-      info: row.info
+      info: row.info,
+      imagen_url: row.imagen_url
     }));
     res.json(productos);
   } catch (error) {
