6fa30494eac4f252759b5fdc625e1f7571251248
I_4m_SilverGWolf <ssoto4383@gmail.com>
2025-07-31 14:29:01 -0600

    Mejora de rutas de imagenes ademas de muestreo correcto de paginas acorde a las modificaciones solicitadas, adicion de CORS

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
+app.use(cors({
+  origin: [process.env.VITE_API_FRONT_URL, 
+    'http://localhost:5174','http://localhost:5173', process.env.VITE_API_URL],
+  methods: ['GET', 'POST']
+}));
 app.use(bodyParser.json({ limit: '10mb' }));
 
 // Rutas de productos
